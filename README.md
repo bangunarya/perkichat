18cbbb5d58a73268834da0440aa0127fd068570d
=benkrisna <benyaminitu@gmail.com>
2020-11-17 18:07:46 +0100

    Tidying up and README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+# PERKI chat
+
 Repositori ini dibuat untuk mengembangkan line chat grup PERKI Aachen
+
+## Configure
+
+Supaya bisa memulai development, pertama:
+
+1. Fork Repository ini kalau belum
+2. Clone Repository ini di local kamu dengan 
+```git clone https://github.com/USERNAME_GITHUB_KAMU/perkichat.git```
+3. Di dalam directory clone itu, run 
+```./configure.py -m ?```
--- chat_group.py
+++ chat_group.py
@@ -1,19 +1,45 @@
 #! /usr/bin/env python3
 import requests, json
 import datetime
-import os
 
 
-token = " "# isi token grup buat tes
+
+##------------------------Global Variables-------------------------------##
+
+URL = "https://notify-api.line.me/api/notify"
+#TODO
+token = "iis96ihAcH7wPoTZQpBymGCcz1JTIVSv75USzXWBoYB"
+#token = "DpOhVLboPeFIS4MbGoALTkyLMyjfyCkLBaK57P16dx8"
+
+#LINE_HEADERS = {'Content-Type':'application/x-www-form-urlencoded',"Authorization":"Bearer "+ token}
+LINE_HEADERS = {'Authorization': 'Bearer ' + token}
+
+def send_message(token, msg, img=None):
+    message = {'message': msg}
+    files = {'imageFile': open(img, 'rb')} if img else None
+    session = requests.Session()
+    resp = session.post(URL, headers=LINE_HEADERS, params=message, files=files)
+    if files:
+        files['imageFile'].close()
+    return resp.status_code
 
 
-kalimat = input(">> ")
 
-LINE_ACCESS_TOKEN = token
-url = "https://notify-api.line.me/api/notify"
-msg = {"message":str(kalimat)} #message    
-LINE_HEADERS = {'Content-Type':'application/x-www-form-urlencoded',"Authorization":"Bearer "+LINE_ACCESS_TOKEN}
-session = requests.Session()
-resp =session.post(url, headers=LINE_HEADERS, data=msg)
-print(resp.text)
+def main():
+    import os
+    import sys
+    import argparse
+    
+    parser = argparse.ArgumentParser(
+            description="Send LINE Message.")
+    
+    parser.add_argument('--img_file', help="Image File to be sent", default = None)
+    parser.add_argument('message')
 
+    args = parser.parse_args()
+    status_code = send_message(token,args.message,args.img_file)
+    print('Status Code = {}'.format(status_code))
+
+
+if __name__ == '__main__':
+    main()
